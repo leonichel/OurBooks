9e14d1d3b1f1640d2fc48fed6ee8b8808d4370f2
leonichel <leonichelg@gmail.com>
2020-03-27 03:22:40 -0300

    v0.4-screen Listen add

--- OurBooks-APP/App.js
+++ OurBooks-APP/App.js
@@ -1,16 +1,24 @@
 // eslint-disable-next-line import/no-unresolved
 import React, { useState, useEffect } from 'react';
-import { Text, View, Image, ScrollView } from 'react-native';
+import { Text, View, Image, ScrollView, TouchableOpacity, Slider } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 import { ListItem, Button, Icon } from 'react-native-elements';
 import { Searchbar } from 'react-native-paper';
+import { Audio } from 'expo-av';
+
+import {
+	responsiveHeight,
+	responsiveWidth,
+	responsiveFontSize
+  	} from "react-native-responsive-dimensions";
 
 import styles from './src/styles';
 
 const Tab = createBottomTabNavigator();
+
 const list = [
 	{
 	  	name: 'O preço do Amanhã',
@@ -72,7 +80,7 @@ const list = [
 		avatar_url: 'https://lojasaraiva.vteximg.com.br/arquivos/ids/12709771/1006567097.jpg?v=637154357742170000',
 		subtitle: 'john Green'
 	},
-   ];
+];
 
 function Explore() {
 
@@ -109,20 +117,100 @@ function Explore() {
 }
 
 function Listen() {
+
+	const [urlSound, setUrlSound] = useState('https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3');
+	const [time, setTime] = useState(0);
+	const [maxTime, setMaxTime] = useState(9000000);
+	const [playing, setPlaying] = useState(false);
+	const [loaded, setLoaded] = useState(false);
+
+	useEffect(() => {
+
+	}, [])
+
+	String.prototype.toHHMMSS = function () {
+		var mill_num = parseInt(this, 10);
+		var sec = Math.floor(mill_num / 1000);
+		var hours   = Math.floor(sec / 3600);
+		var minutes = Math.floor((sec - (hours * 3600)) / 60);
+		var seconds = sec - (hours * 3600) - (minutes * 60);
+	
+		if (hours   < 10) {hours   = "0"+hours;}
+		if (minutes < 10) {minutes = "0"+minutes;}
+		if (seconds < 10) {seconds = "0"+seconds;}
+
+		return hours+':'+minutes+':'+seconds;
+	}
+
   	return (
-		<View style={ styles.topInfo }>
-			<View style={ styles.topInfoTexts }>
-				<View style={ styles.topInfoTextsTitle }>
-					<Text style={ styles.topInfoTextsTitleFont }>OurBooks - Always Free</Text>
+		<SafeAreaView forceInset={{top: 'always'}} style={ styles.container }>
+			<View style={ styles.topInfo }>
+				<View style={ styles.topInfoTexts }>
+					<View style={ styles.topInfoTextsTitle }>
+							<Text multiline = {true} numberOfLines = {4} style={ styles.topInfoTextsTitleFont }>A CULPA É DAS ESTRELAS</Text>
+					</View>
+					<View style={ styles.topInfoTextsSubtitle }>
+					<Text style={ styles.topInfoTextsSubtitleFont }>John Green</Text>
+					</View>
+				</View>
+				<View style={ styles.topInfoImg }>
+					<Image
+					style={ styles.topInfoImg }
+					source={require('./src/assets/icon.png')}
+					/>
 				</View>
 			</View>
-			<View style={ styles.topInfoImg }>
+			<View style={styles.MiddleInfo}>
 				<Image
-				style={ styles.topInfoImg }
-				source={require('./src/assets/icon.png')}
+					style={ styles.imageMiddleInfo }
+					source={{uri:'https://fanficcao.files.wordpress.com/2017/09/picsart_09-16-01-19-19.jpg'}}
 				/>
+				<Slider
+					style={ {marginTop: responsiveHeight(2) }}
+					thumbTintColor={'#000'}
+					minimumTrackTintColor={'#000'}
+					value={ time }
+					onValueChange={ value => {setTime(value)} }
+					maximumValue={ maxTime }
+				/>
+				<Text>{String(time).toHHMMSS()}</Text>
+				<View style={styles.controlBtn}>
+					<TouchableOpacity style={{marginRight: responsiveWidth(5)}}>
+						<MaterialCommunityIcons
+							name="skip-previous"
+							size={55}
+							color="black"
+						/>
+						</TouchableOpacity>
+						{ (playing)
+						? (<TouchableOpacity 
+						style={{marginRight: responsiveWidth(5)}}
+						onPress={() => { handlePlayPause() }}>
+						<MaterialCommunityIcons
+							name="pause-circle"
+							size={95}
+							color="black"
+						/>
+						</TouchableOpacity>)
+						: (<TouchableOpacity
+						style={{marginRight: responsiveWidth(5)}}
+						onPress={() => { handlePlayPause() }}>
+						<MaterialCommunityIcons
+							name="play-circle"
+							size={95}
+							color="black"
+						/>
+						</TouchableOpacity>) }
+					<TouchableOpacity>
+						<MaterialCommunityIcons
+							name="skip-next"
+							size={55}
+							color="black"
+						/>
+					</TouchableOpacity>
+				</View>
 			</View>
-		</View>
+		</SafeAreaView>
 	);
 }
 
@@ -132,30 +220,29 @@ export default function App() {
 	  <NavigationContainer>
 		<Tab.Navigator
 		  	screenOptions={({ route }) => ({
-				tabBarIcon: ({ focused, color, size }) => {
-				let iconName;
-			  if (route.name === 'Explorar') {
-					iconName = focused
-				  	? 'ios-apps'
-					: 'ios-apps';
-				} 
-				else if (route.name === 'Ouvir') {
-					iconName = focused 
-					? 'ios-play-circle' 
-					: 'ios-play-circle';
-				}
-
-				return <Ionicons name={iconName} size={size} color={color} />;
-				},
-				})}
-				tabBarOptions={{
-						activeTintColor: '#000',
-						inactiveTintColor: 'gray',
-						style: {elevation:0, borderTopWidth: 0, shadowOpacity:0, backgroundColor: '#E9EEF6'},
-				}}
-			>
-		  	<Tab.Screen name="Explorar" component={Explore} />
-		  	<Tab.Screen name="Ouvir" component={Listen} /> 
+			tabBarIcon: ({ focused, color, size }) => {
+			let iconName;
+			if (route.name === 'Explorar') {
+				iconName = focused
+				? 'ios-apps'
+				: 'ios-apps';
+			} 
+			else if (route.name === 'Ouvir') {
+				iconName = focused 
+				? 'ios-play-circle' 
+				: 'ios-play-circle';
+			}
+			return <Ionicons name={iconName} size={35} color={color} />;
+			},
+			})}
+			tabBarOptions={{
+					activeTintColor: '#000',
+					inactiveTintColor: 'gray',
+					style: {elevation:0, borderTopWidth: 0, shadowOpacity:0, backgroundColor: '#E9EEF6'},
+			}}
+		>
+		<Tab.Screen name="Explorar" component={Explore} />
+		<Tab.Screen name="Ouvir" component={Listen} /> 
 		</Tab.Navigator>
 	  	</NavigationContainer>
 	);
--- OurBooks-APP/src/styles.js
+++ OurBooks-APP/src/styles.js
@@ -6,65 +6,90 @@ import {
   	} from "react-native-responsive-dimensions";
 
 const styles = StyleSheet.create({
+
     container: {
         flex: 1,
-        width: '100%',
+        width: responsiveWidth(100),
         justifyContent: 'flex-start',
         alignItems: 'center',
         backgroundColor: '#E9EEF6',
     },
 
     topInfo: {
-        height: 100,
-        width: '90%',
+        height: responsiveHeight(25),
+        width: responsiveWidth(90),
         flexDirection: 'row',
-        marginTop: 50,
+		marginTop: responsiveHeight(5),
+		marginLeft: responsiveWidth(6),
+		// backgroundColor: '#a20'
     },
 
     topInfoImg: {
-        height: 100,
-        width: 100,
-        borderRadius: 50,
+        height: responsiveHeight(15),
+        width: responsiveWidth(25),
+		borderRadius: 50,
+		marginRight: responsiveHeight(2),
+		// backgroundColor: '#f50'
     },
 
     topInfoTexts: {
-        height: 100,
+        height: responsiveHeight(30),
         flex: 1,
-        paddingLeft: 10,
+		marginLeft: responsiveHeight(2),
+		marginBottom: responsiveHeight(2),
+		// backgroundColor: '#f99',
     },
 
     topInfoTextsTitle: {
-        height: 40,
-        width: '100%',
+        height: responsiveHeight(12),
+		width: '100%',
 		justifyContent: 'center',
+		marginBottom: responsiveHeight(0.5),
+		// backgroundColor: '#f0f',
     },
 
     topInfoTextsSubtitle: {
-        height: 60,
+        height: responsiveHeight(9.5),
 		width: '100%',
-		marginRight: '3%',
+		// backgroundColor: '#f10',
 	},
 
     topInfoTextsTitleFont: {
-        fontSize: 22,
+        fontSize: responsiveFontSize(3.5),
 		fontWeight: 'bold',
 		justifyContent: 'center',
     },
 
     topInfoTextsSubtitleFont: {
-		fontSize: 18,
+		fontSize: responsiveFontSize(2.5),
 		justifyContent: 'center',
 	},
 	
 	MiddleInfo: {
-        flex:1,
+        flex: 1,
         width: responsiveWidth(90),
-		borderRadius: 10,
+		marginBottom: responsiveHeight(5),
+		justifyContent: 'center',
+		alignContent: 'center',
+		// backgroundColor: '#a91'
 	},
-	
-	mapStyle: {
-		flex:1,
+
+	imageMiddleInfo : {
+		flex: 1,
+		width: responsiveWidth(90),
+		borderRadius: 15
+		// backgroundColor: '#f50'
 	},
+
+	controlBtn: {
+		marginTop: responsiveHeight(2),
+        height: responsiveHeight(10),
+        width: responsiveWidth(90),
+        flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center',
+		//backgroundColor: '#a20'
+    },
 })
 
 export default styles;
